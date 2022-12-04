75e86ae76ce7f2e368b9771164c46404dcd8a160
Jon Ekdahl <jon.ekdahl@valtech.com>
2022-12-04 17:04:01 +0100

    Day 04, with sets

--- 04/day.fsx
+++ 04/day.fsx
@@ -1,8 +1,9 @@
-type Range = int * int
+type Range = Set<int>
 
 let parseRange (s: string): Range =
     let parts = s.Split "-"
-    (int parts[0]), (int parts[1])
+    [int parts[0] .. (int parts[1])]
+    |> Set
 
 let parsePair (s: string) =
     let parts = s.Split ","
@@ -12,22 +13,20 @@ let assignments =
     System.IO.File.ReadAllLines("input")
     |> Seq.map parsePair
 
-let fullyContains (r1: Range) (r2: Range) =
-    let start1, end1 = r1
-    let start2, end2 = r2
-    (start2 >= start1 && start2 <= end1) && (end2 >= start1 && end2 <= end1)
+let fullyContainsEachOther (r1: Range) (r2: Range) =
+    Set.isSubset r1 r2 || Set.isSubset r2 r1
 
 assignments
-|> Seq.filter (fun (r1, r2) -> fullyContains r1 r2 || fullyContains r2 r1)
+|> Seq.filter (fun (r1, r2) -> fullyContainsEachOther r1 r2)
 |> Seq.length
 |> printfn "Part 1: %A"
 
-let isWithin (r1: Range) (r2: Range) =
-    let start1, end1 = r1
-    let start2, end2 = r2
-    (start2 >= start1 && start2 <= end1) || (end2 >= start1 && end2 <= end1)
+let overlapsEachOther (r1: Range) (r2: Range) =
+    Set.intersect r1 r2
+    |> Set.isEmpty
+    |> not
 
 assignments
-|> Seq.filter (fun (r1, r2) -> isWithin r1 r2 || isWithin r2 r1)
+|> Seq.filter (fun (r1, r2) -> overlapsEachOther r1 r2)
 |> Seq.length
 |> printfn "Part 2: %A"
