4639288327c5c0ea5fe44eb5f7f582228bbc081c
Jon Ekdahl <jon.ekdahl@valtech.com>
2022-12-18 14:50:54 +0100

    Day 08, reuse more functions in both parts

--- 08/day.fsx
+++ 08/day.fsx
@@ -3,24 +3,6 @@ let grid =
     |> Seq.map (fun s -> s |> Seq.map System.Globalization.CharUnicodeInfo.GetDigitValue)
     |> array2D
 
-let isVisibleRow (tree: int) (other: int[]) =
-    tree > (other |> Array.max)
-
-let isVisible (grid: int[,]) (x, y) =
-    let minIndex = 0
-    let maxIndex = (grid |> Array2D.length1) - 1
-    let tree = grid[x,y]
-    if x = minIndex || x = maxIndex || y = minIndex || y = maxIndex then 
-        true
-    else
-        seq {
-            grid[x, ..y-1]  // top
-            grid[x, y+1..]  // bottom
-            grid[..x-1, y]  // left
-            grid[x+1.., y]  // right
-        }
-        |> Seq.exists (isVisibleRow tree)
-
 let coordinates (grid: int[,]) =
     let gridLength = grid |> Array2D.length1
     seq { 
@@ -29,10 +11,32 @@ let coordinates (grid: int[,]) =
                 yield x, y
     }
 
-coordinates grid
-|> Seq.filter (isVisible grid)
-|> Seq.length
-|> printfn "Part 1: %d"
+let innerCoordinates (grid: int[,]) =
+    let gridLength = grid |> Array2D.length1
+    let maxIndex = gridLength - 1
+    coordinates grid
+    |> Seq.filter (fun (x, y) -> x <> 0 && x <> maxIndex && y <> 0 && y <> maxIndex)
+
+let isVisibleRow (tree: int) (other: int[]) =
+    tree > (other |> Array.max)
+
+let directionsFrom (grid: int[,]) x y =
+    seq {
+        grid[x, ..y-1] |> Array.rev  // up
+        grid[x, y+1..]               // down
+        grid[..x-1, y] |> Array.rev  // left
+        grid[x+1.., y]               // right
+    }
+
+let isVisible (grid: int[,]) (x, y) =
+    let minIndex = 0
+    let maxIndex = (grid |> Array2D.length1) - 1
+    let tree = grid[x,y]
+    if x = minIndex || x = maxIndex || y = minIndex || y = maxIndex then 
+        true
+    else
+        directionsFrom grid x y
+        |> Seq.exists (isVisibleRow tree)
 
 let viewingDistance (tree: int) (others: int[]) =
     let lower =
@@ -43,22 +47,14 @@ let viewingDistance (tree: int) (others: int[]) =
 
 let scenicScore (grid: int[,]) (x, y): int =
     let tree = grid[x,y]
-
-    seq {
-        grid[x, ..y-1] |> Array.rev  // up
-        grid[x, y+1..]               // down
-        grid[..x-1, y] |> Array.rev  // left
-        grid[x+1.., y]               // right
-    }
+    directionsFrom grid x y
     |> Seq.map (viewingDistance tree)
     |> Seq.fold (fun acc x -> acc * x) 1
 
-
-let innerCoordinates (grid: int[,]) =
-    let gridLength = grid |> Array2D.length1
-    let maxIndex = gridLength - 1
-    coordinates grid
-    |> Seq.filter (fun (x, y) -> x <> 0 && x <> maxIndex && y <> 0 && y <> maxIndex)
+coordinates grid
+|> Seq.filter (isVisible grid)
+|> Seq.length
+|> printfn "Part 1: %d"
 
 innerCoordinates grid
 |> Seq.map (scenicScore grid)
