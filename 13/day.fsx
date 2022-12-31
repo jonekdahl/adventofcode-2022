2a81f5d8aae078e9bed6ebf80fad466c9b051afa
Jon Ekdahl <jon.ekdahl@valtech.com>
2022-12-31 16:40:38 +0100

    Day 13, use integers directly instead of DU

--- 13/day.fsx
+++ 13/day.fsx
@@ -30,37 +30,32 @@ let packets =
         | [| p1; p2 |] -> PacketData.parse p1, PacketData.parse p2
         | other -> failwithf "Unexpected input: %A" other)
 
-type Order =
-    | LeftSmaller
-    | Equal
-    | RightSmaller
-
-let rec compare (p1: PacketData, p2: PacketData): Order =
+let rec compare (p1: PacketData) (p2: PacketData): int32 =
     match p1, p2 with
     | Int left, Int right ->
         if left < right then
-            LeftSmaller
+            -1
         else if right < left then
-            RightSmaller
+            1
         else
-            Equal
+            0
     | PacketList data1, PacketList data2 ->
         match data1, data2 with
-        | [], [] -> Equal
-        | [], _ -> LeftSmaller
-        | _, [] -> RightSmaller
+        | [], [] -> 0
+        | [], _ -> -1
+        | _, [] -> 1
         | h1 :: t1,  h2 :: t2 ->
-            match compare (h1, h2) with
-            | LeftSmaller | RightSmaller as x -> x
-            | Equal -> compare (PacketList t1, PacketList t2)
+            match compare h1 h2 with
+            | 0 -> compare (PacketList t1) (PacketList t2)
+            | other -> other
     | PacketList _, Int _ ->
-        compare (p1, PacketList [p2])
+        compare p1 (PacketList [p2])
     | Int _, PacketList _ ->
-        compare (PacketList [p1], p2)
+        compare (PacketList [p1]) p2
 
 packets
 |> Seq.mapi (fun idx pd -> idx + 1, pd)
-|> Seq.filter (fun (_idx, pd) -> (compare pd) = LeftSmaller)
+|> Seq.filter (fun (_idx, (p1, p2)) -> (compare p1 p2) = -1)
 |> Seq.sumBy fst
 |> printfn "Part 1: %A"
 
@@ -72,17 +67,11 @@ let decoderKey (ps: PacketData list) =
     let idx2 = ps |> List.findIndex (fun p -> p = divider2)
     (idx1 + 1) * (idx2 + 1)
 
-let packetDataComparer p1 p2 =
-    match compare (p1, p2) with
-    | LeftSmaller -> -1
-    | RightSmaller -> 1
-    | Equal -> 0
-
 packets
 |> Seq.map (fun (p1, p2) -> seq { p1; p2 })
 |> Seq.concat
 |> Seq.append (seq { divider1 ; divider2 })
-|> Seq.sortWith packetDataComparer
+|> Seq.sortWith compare
 |> List.ofSeq
 |> decoderKey
 |> printfn "Part 2: %A"
